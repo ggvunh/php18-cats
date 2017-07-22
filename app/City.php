58d6063e0f7023a6cb8425ff000fb5edd0241d3d
vunh <vunh@greenglobal.vn>
2017-07-22 09:46:29 +0700

    code b

--- app/City.php
+++ app/City.php
@@ -9,4 +9,9 @@ class City extends Model
     protected $fillable = ['name'];
 
     protected $table = 'cities';
+
+    public function codeB()
+    {
+        return 'day la code cua b'; 
+    }
 }
